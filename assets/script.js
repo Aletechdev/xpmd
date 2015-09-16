3122b0d0d681dba310eb014f1222d444a7a1ec19
Justin Lu <justinlu10@gmail.com>
2015-09-16 14:48:02 -0700

    use FileSaver.min.js and CSV.min.js

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
     
     $('#test2').select2();
     $( "#datepicker" ).datepicker();
+    
+    $("#submitForm").click(function(){
+        saveFile();
+    });
+    
 });
+
+function saveFile(){
+    
+    var data = [[1850, 20, 0, 1, 1017281], [1850, 20, 0, 2, 1003841]];
+    var csv = []
+    csv.push(new CSV(data, { header: ["year", "age", "status", "sex", "population"] }).encode());
+    var file = new Blob(csv, {type: "text/plain;charset=utf-8"});
+    saveAs(file, "example.csv");
+    
+}
