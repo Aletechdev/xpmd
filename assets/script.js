070ffa7d8366b0fddf774eb7c62082cfaf32ad61
Justin Lu <justinlu10@gmail.com>
2015-09-17 16:06:48 -0700

    added complete list of metadata input

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-    $('#test1').select2();
+    
+    var data = [['date', 'datepicker', true, 'date']]
+
+    $('#test1').select2({
+        placeholder: "Select Sequencers",
+        allowClear: true
+    });
     
     $('#test2').select2();
     $( "#datepicker" ).datepicker();
@@ -12,10 +18,15 @@ $(document).ready(function(){
 
 function saveFile(){
     
-    var data = [[1850, 20, 0, 1, 1017281], [1850, 20, 0, 2, 1003841]];
+    var data = [[$('#datepicker').val(), 20, 0, 1, 1017281], [1850, 20, 0, 2, 1003841]];
     var csv = []
     csv.push(new CSV(data, { header: ["year", "age", "status", "sex", "population"] }).encode());
     var file = new Blob(csv, {type: "text/plain;charset=utf-8"});
     saveAs(file, "example.csv");
     
 }
+
+function getValue(){
+        
+}
+
