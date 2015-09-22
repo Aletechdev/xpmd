16dea861d3161b03ee149cacf9d8b5f00ebc43a3
Justin Lu <justinlu10@gmail.com>
2015-09-22 13:18:20 -0700

    add array and incomplete inputs alert

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,44 @@
-var data = [['date', 'datepicker', true, 'date'], ['data type', 'data-type', 'true', 'input'] ]
+var data = [['Date', 'datepicker', true, 'date'], 
+['Data Type', 'data-type', true, 'input'], ['Raw File Format', 'raw-file-format', true, 'dropdown'], ['Organism', 'organism', true, 'dropdown'], 
+['Parent Strain', 'parent-strain', true, 'input'], ['Strain ID', 'strain-id', true, 'input'], ['Mutations', 'mutations', true, 'input'], 
+['Antibody', 'antibody', false, 'input'], ['Growth Stage', 'growth-stage', true, 'input'], ['Oxygen Availability', 'oxygen-availability', true, 'input'], 
+['Sample Preparation Details', 'sample-preparation-details', false, 'input'], ['Base Media', 'base-media', true, 'input'], ['Carbon Source(s)', 'carbon-source', true, 'multi-dropdown'], 
+['Carbon Source(s)', 'carbon-source-custom', false, 'input'], ['Concentration for each carbon source (g/L)', 'concentration', true, 'input-number'], 
+['Nitrogen Source', 'nitrogen-source', true, 'multi-dropdown'],['Nitrogen Source', 'nitrogen-source-custom', false, 'input'], ['Phosphorous Source', 'phosphorous-source', true, 'multi-dropdown'],
+['Phosphorous Source', 'phosphorous-source-custom', false, 'input'], ['Sulphur Source', 'sulphur-source', true, 'multi-dropdown'],['Sulphur Source', 'sulphur-source-custom', false, 'input'], 
+['Antibiotic Resistance', 'antibiotic-resistance', true, 'input'], ['Replicates', 'replicates', false, 'input'], ['Other Experimental Variables', 'other-experimental-variables', false, 'input'], 
+['Sequencer', 'sequencer', false, 'multi-dropdown'], ['Sequencer', 'sequencer-custom', false, 'input'], ['Read Length', 'read-length', false, 'input'], ['Illumina Kit Details', 'illumina-kit-details', false, 'input'], 
+['Experiment Summary and Hypothesis', 'experiment-summary', false, 'textarea'], ['Experimentalist', 'experimentalist', false, 'input'], ['Experimentalist Email', 'experimentalist-email', false, 'input'], 
+['Grant ID', 'grant-id', false, 'input'], ['Kit Used to Isolate DNA/RNA', 'isolate-kit', false, 'input'], ['SOP Title', 'sop-title', false, 'input'], ['Alignment Algorithm and Options', 'alignment-algorithm', false, 'input'], 
+['Reference Sequence', 'reference-sequence', false, 'input'], ['Other Processing Details', 'other-processing-details', false, 'input'], ['GEO Submission', 'geo-submission', false, 'input'], ['Author(s)', 'author', true, 'input'],  
+['State of Preculture', 'state-of-preculture', false, 'input'], ['Media of Preculture', 'media-of-preculture', false, 'input'], ['Flasks Since Frozen Stock', 'flasks', false, 'input'], ['Time Point', 'time-point', false, 'input'] ]
 
 $(document).ready(function(){
 
-    $('#sequencer').select2({
+    /*$('#sequencer').select2({
         placeholder: "Select Sequencers",
         allowClear: true
     });
     
     $('#test2').select2();
-    $( "#datepicker" ).datepicker();
-    var array = []
-    for(int i = 0; i< data.length(); i++){
-        array.push([data[i][0], getValue(data[1])]);
-    }
+    $( "#datepicker" ).datepicker();*/
 
+    for(i=0;i<data.length;i++){
+        bindFunction(data[i]);
+    }
     $("#submitForm").click(function(){
+        
+        var array = []
+        for(i = 0; i< data.length; i++){
+            var val = getValue(data[i]);
+            if(val == 'Empty'){
+                alertIncomplete(data[i][1]);
+                return;
+            }
+            else{
+                array.push([data[i][1], val]);
+            }
+        }
         saveFile(array);
     });
     
@@ -22,8 +46,6 @@ $(document).ready(function(){
 
 function saveFile(array){
     
-    //var data = [[$('#datepicker').val(), 20, 0, 1, 1017281], [1850, 20, 0, 2, 1003841]];
-    //var array = [[data[1][0], getValue(data[1])]];
     var csv = []
     csv.push(new CSV(array).encode());
     var file = new Blob(csv, {type: "text/plain;charset=utf-8"});
@@ -32,24 +54,27 @@ function saveFile(array){
 }
 
 function alertIncomplete(input){
-    alert( input + 'is required')
+    alert( input + ' is required')
 }
 
 function getValue(data){
+       var value = $('#'+data[1]).val();  
        if(data[2] == true){
-            if ($('#'+data[1]).val() == ''){
-                alertIncomplete(data[0]);    
+            if (value  == ''){
+                //alertIncomplete(data[0]);  
+                return 'Empty';
             }
             else{
-                return $('#'+data[1]).val();
+                return value;
             }
        }
        else{
-            return $('#'+data[1]).val();
+            return value;
        }
 }
+
 function bindFunction(data){
-       if(data[3] == 'select2'){
+       if(data[3] == 'multi-dropdown'){
             $('#'+data[1]).select2();
        }
        else if (data[3] == 'date'){
