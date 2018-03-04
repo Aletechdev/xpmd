30b35531d6c9029845d321e1ea51cd86cff92c77
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2018-03-04 10:08:44 -0800

    Updated lists cycles and read length list.
    Removed incorrect additions to the cycles list and added to read length list.

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ var electron_acceptor_options= ['O2', 'NO3', 'SO4']
 var antibiotic_options = ['Kanamycin', 'Spectinomycin', 'Streptomycin', 'Ampicillin', 'Carbenicillin', 'Bleomycin', 'Erythromycin', 'Polymyxin B', 'Tetracycline', 'Chloramphenicol']
 var lib_prep_options = ['Nextera XT', 'KAPA HyperPlus', 'KAPA Stranded RNA-seq']
 var lib_prep_manufacturer_options = ['Illumina', 'Kapa']
-var lib_prep_cycle_options = ['31 Cycle', '36 Cycle', '50 Cycle', '62 Cycle', '76 Cycle', '100 Cycle', '150 Cycle', '300 Cycle', '500 Cycle', '600 Cycle']
+var lib_prep_cycle_options = ['50 Cycle', '76 Cycle', '150 Cycle', '300 Cycle', '500 Cycle', '600 Cycle']
 var read_type_options = ['Single-end reads', 'Paired-end reads']
-var read_length_options = ['31', '36', '50', '62', '76', '100', '151', '301']
+var read_length_options = ['31', '36', '50', '62', '76', '100', '151', '251', '301']
 
 var data = [
   { label: 'Creator (Name)',
